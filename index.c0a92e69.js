b69a1a5174553085e5d8f1030850292621a9cbbc
newqou <137334905+newqou@users.noreply.github.com>
2023-12-02 18:06:09 +0200

    make build

---
+++ index.c0a92e69.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,r=e[0].children.length-1,l=!0,t=!1,n=void 0;try{for(var o,a=e[Symbol.iterator]();!(l=(o=a.next()).done);l=!0){var c=o.value,i=c.cells[1],u=c.cells[r];c.insertBefore(i.cloneNode(!0),u)}}catch(e){t=!0,n=e}finally{try{l||null==a.return||a.return()}finally{if(t)throw n}}
+//# sourceMappingURL=index.c0a92e69.js.map
